4d1d0423cc983fffa05f92b43edea6cf07b7d6e0
Hlebnica <49001181+Hlebnica@users.noreply.github.com>
2022-03-01 22:31:26 +0500

    11

--- Lab_3/PR_3_Sets/Program.cs
+++ Lab_3/PR_3_Sets/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+/*
+Пусть дан текст, состоящий из 
+строчных латинских букв и цифр. Определить, 
+каких букв – гласных или согласных – больше в этом тексте.
+*/
 
 namespace PR_3_Sets
 {
     class Program
     {
+        private static HashSet<char> _vowels = new HashSet<char>("аеиоуыэюя");
+        private static HashSet<char> _consonants = new HashSet<char>("бвгджзйклмнпрстфхцчшщ");
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string s = "поэты и сами ещё ни разу не договорились о том, что такое поэзия.";
+            int d = s.Aggregate(0, (y, x) => y + (_vowels.Contains(x) ? 1 : _consonants.Contains(x) ? -1 : 0));
+            Console.WriteLine(d < 0 ? "согласных больше" : d == 0 ? "одинаково" : "гласных больше");
         }
     }
 }
