0513f3e31bbcfa852836acec66676f151886dfc9
Hlebnica <49001181+Hlebnica@users.noreply.github.com>
2022-03-27 18:58:39 +0500

    789

--- Lab_3/PR_3_Queue/Program.cs
+++ Lab_3/PR_3_Queue/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+
+/*Удалить первый и последний элементы очереди целых чисел.*/
 
 namespace PR_3_Queue
 {
@@ -6,7 +9,62 @@ namespace PR_3_Queue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите количество чисел:");
+            int n = Convert.ToInt32(Console.ReadLine()); // Считываем количество чисел в очереди
+            
+            Queue<int> numbers = new Queue<int>();
+            Queue<int> temp = new Queue<int>();
+            
+            Random rnd = new Random();
+            
+            for (int i = 0; i < n; i++) // Добавление случайных чисел в очередь
+            {
+                numbers.Enqueue(rnd.Next(-10, 10));
+            }
+
+            Queue<int> newNumbers = new Queue<int>(numbers); // Копируем содержимое основной очереди, чтобы показать хранящиеся элементы
+            var k = n;
+            Console.WriteLine("Содержимое очереди:");
+            while (k > 0)
+            {
+                var num = newNumbers.Dequeue();
+                Console.WriteLine(num);
+                k--;
+            }
+            
+            k = n;
+            Console.WriteLine("Содержимое очереди без последнего элемента:");
+            while (k > 1)
+            {
+                var num = numbers.Dequeue();
+                Console.WriteLine(num);
+                temp.Enqueue(num); // перенос в temp всех элементов очереди кроме последнего
+                k--;
+            }
+            
+            numbers.Clear(); // очищаем очередь от оставшегося числа
+            temp.Dequeue(); // убираем первый элемент из временного стека
+            
+            k = n - 2;
+            while (k > 0) // перенос из temp в основную очередь
+            {
+                var num = temp.Dequeue();
+                numbers.Enqueue(num);
+                k--;
+            }
+            
+            Console.WriteLine("Содержимое очереди без первого и последнего элемента:");
+            k = n - 2;
+            while (k > 0) // Вывод чисел из основной очереди
+            {
+                var num = numbers.Dequeue(); 
+                Console.WriteLine(num);
+                k--;
+            }
+            
+
+
+
         }
     }
 }
