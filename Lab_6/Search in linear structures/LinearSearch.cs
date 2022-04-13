61a6031b46e93c54648b263e3bcb663350e6bf62
Hlebnica <49001181+Hlebnica@users.noreply.github.com>
2022-04-13 14:08:31 +0500

    111

---
+++ Lab_6/Search in linear structures/LinearSearch.cs
@@ -0,0 +1,13 @@
+﻿namespace Search_in_linear_structures
+{
+    public class LinearSearch
+    {
+        static int LineSearcher()
+        {
+            int counter = 0;
+        
+            return -1;
+        }
+        
+    }
+}
---
+++ Lab_6/Search in linear structures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Search_in_linear_structures
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
