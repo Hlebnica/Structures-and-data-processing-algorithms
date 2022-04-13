e59e009f4d732a88f82d86b4dc12e8e9e1e06fdf
Hlebnica <49001181+Hlebnica@users.noreply.github.com>
2022-04-14 00:56:20 +0500

    1243124

--- Lab_6/Search in linear structures/Program.cs
+++ Lab_6/Search in linear structures/Program.cs
@@ -2,10 +2,24 @@
 
 namespace Search_in_linear_structures
 {
-    internal class Program
+    class Program
     {
+        
         public static void Main(string[] args)
         {
+            Console.WriteLine("Введите размер массива ");
+            int arrSize = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[arrSize];
+            Random rnd = new Random();
+            
+            for (int i = 0; i < arrSize; i++)
+            {
+                array[i] = rnd.Next(-10, 10);
+                Console.Write(array[i] + " ");
+            }
+            
+            
+            Array.Sort(array);
             
         }
     }
