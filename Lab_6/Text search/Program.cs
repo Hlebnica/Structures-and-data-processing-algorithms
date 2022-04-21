f73c79ecd08acdae22cbc43c0dc92650e8f8cc6c
Дмитрий Старостенок <49001181+Hlebnica@users.noreply.github.com>
2022-04-21 15:34:26 +0500

    123

---
+++ Lab_6/Text search/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+/*3. Поиск в тексте
+В текстовом файле хранится текст. Осуществить прямой поиск введенного 
+пользователем слова с использованием алгоритма Боуера-Мура*/
+
+namespace Text_search
+{
+    class Program
+    {
+        private static string GetTextFromFile(string name)
+        {
+            string text;
+            using (StreamReader reader = new StreamReader(name))
+            {
+                text = reader.ReadLine();
+            }
+
+            return text;
+        }
+        
+        
+        
+        public static void Main(string[] args)
+        {
+            string text = GetTextFromFile("text.txt");
+            Console.WriteLine(text);
+        }
+    }
+}
