deee1c9621fcd53e758afd471fa01779525d33fa
Hlebnica <49001181+Hlebnica@users.noreply.github.com>
2022-04-24 22:21:55 +0500

    789

--- Lab_4/DataHashing_1/Program.cs
+++ Lab_4/DataHashing_1/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections;
+using System.IO;
 
-/*Составьте хеш-таблицу, содержащую буквы и количество 
-    их вхождений во введенной строке. Вывести таблицу на экран.
-    Осуществить поиск введенной буквы в хеш-таблице.*/
+/*
+Составьте хеш-таблицу, содержащую буквы и количество 
+их вхождений во введенной строке. Вывести таблицу на экран.
+Осуществить поиск введенной буквы в хеш-таблице.
+*/
 
 namespace DataHashing_1
 {
@@ -10,6 +14,17 @@ namespace DataHashing_1
     {
         public static void Main(string[] args)
         {
+            Hashtable letters = new Hashtable();
+            
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            
+            var chars = str?.ToCharArray();
+            foreach (var c in chars)
+            {
+                Console.Write(c + " ");
+            }
+            
         }
     }
 }
---
+++ Lab_4/DataHashing_3/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DataHashing_3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataHashing_3")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("D07225D8-AB62-4592-997B-0BD641B221BE")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
